98b088c88861a8534c3b259b2fed899ae7520581
Sam Bennett <sbennett.wm@gmail.com>
2015-09-03 14:57:21 -0400

    first commit

---
+++ eager-app.js
@@ -0,0 +1,47 @@
+
+// IIFE
+(function() {
+
+    var options, text, textEl, fontSize, addStyles;
+
+    // this is the object containing the user data from the install UI screen
+    options = INSTALL_OPTIONS;
+    text = options.text;
+
+    // uppercase if option is true
+    if(options.uppercase) {
+        text = text.toUpperCase();
+    }
+
+    textEl = document.createElement('text');
+    textEl.innerHTML = text;
+
+    // determine font size from user inputs
+    switch(options.size) {
+        case 'small':
+            fontSize = 12;
+            break;
+        case 'medium':
+            fontSize = 18;
+            break;
+        case 'large':
+            fontSize = 24;
+            break;
+    };
+
+    addStyles = function() {
+        var style = document.createElement('style');
+        style.innerHTML = '' +
+            'text {' +
+                'color: ' + options.color + ' !important' +
+                'font-size: ' + fontSize + ' !important' +
+            '}';
+        document.body.appendChild(style);
+    };
+
+    // after DOM renders, load app
+    document.addEventListener('DOMContentLoaded', function() {
+        addStyles();
+    });
+
+})();
